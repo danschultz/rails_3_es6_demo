b789e2a6e067bdffd2b50ba43eaaf3c969367072
Dan Schultz <schultz.t.dan@gmail.com>
2016-05-07 00:09:30 -0700

    Add source path for Babel

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,14 @@
+require 'babel/transpiler'
+
 class SystemES6Processor < Sprockets2::ES6Processor
   def configuration
     {
       'modules' => "system",
-      'moduleIds' => true
+      'moduleIds' => true,
+      'externalHelpers' => true
     }
   end
 end
 
+Rails.application.assets.append_path Babel::Transpiler.source_path
 Rails.application.assets.register_engine('.es6', SystemES6Processor)
